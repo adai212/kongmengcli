e18f3f417c6b3da7fd706d2bad1ed753a6b9d6dd
kongmeng <987999409@qq.com>
2018-11-25 15:52:00 +0800

    add test-vue

---
+++ test-vue/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Hello,World !
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {};
+        },
+        props:[]
+    }
+</script>
+<style lang="less">
+    
+</style>
